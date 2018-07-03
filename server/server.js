dd9560cddc479d28497da147c97f8192303f760b
Sviatlana <SviatlanaGordienok@gmail.com>
2018-07-03 13:48:40 +0100

    Add DELETE /todos/id, mongoose-remove.js

--- server/server.js
+++ server/server.js
@@ -8,6 +8,9 @@ var{User} = require('./models/user')
 
 var app = express();
 
+//heroku
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -41,12 +44,34 @@ app.get('/todos/:id', (req, res) =>{
     return res.status(404).send();
     }
       res.send({todo});
-  }).catch((error) => {res.status(400).send();
+  })
+  .catch((error) => {
+    res.status(400).send();
   });
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.delete('/todos/:id', (req, res) =>{
+ var id = req.params.id;
+ if(!ObjectID.isValid(id)){
+ return res.status(404).send();
+ }
+ Todo.findByIdAndRemove(id).then((todo) =>{
+   if(!todo){
+     return res.status(404).send();
+   }
+   res.status(200).send({todo});
+   // his line:
+   // res.send(todo);
+ })
+ .catch((error) => {
+   res.status(400).send();
+ });
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Started up at port ${port}.`);
 });
 
 module.exports = {app};
